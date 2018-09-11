5e8bb52ce09ecfc3f63300f5c09a7b8cab629ddf
Dennis Ruto <34184017+rutodeno@users.noreply.github.com>
2018-09-11 12:12:05 -0400

    Added function listToArray that return an array.

--- list.js
+++ list.js
@@ -9,6 +9,17 @@ function arrayToList (array) {
     	return { value : array.shift(), rest: arrayToList(array)}
     }
 }
+function  listToArray (list) {
+  let newArray = [];
+  for (let node = list; node ; node = node.rest) {
+  	let {value} = node;
+  	newArray.push (value);
+  }
+  return newArray;
+}
+
 
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30, 40])));
+// [10, 20, 30, 40]
