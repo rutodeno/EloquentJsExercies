ce9d091076743e023389308fe74da8d4a42c0f41
Dennis Ruto <34184017+rutodeno@users.noreply.github.com>
2018-09-17 13:45:15 -0400

    updated list.js

--- chapter4_DataStructures_Objects_and_Arrays/list.js
+++ chapter4_DataStructures_Objects_and_Arrays/list.js
@@ -19,7 +19,20 @@ function  listToArray (list) {
 }
 
 
+function prepend(number, list) { 
+  return { value : number, rest: list}
+}
+
+function nth (list, number) {
+  let listArray = listToArray(list);
+  return listArray[number];
+}
+
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
-console.log(listToArray(arrayToList([10, 20, 30, 40])));
-// [10, 20, 30, 40]
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
