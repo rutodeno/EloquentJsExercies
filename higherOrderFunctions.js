f86ae80f343d3ad5a516cbdcfff234e8505fc113
Dennis Ruto <34184017+rutodeno@users.noreply.github.com>
2018-09-20 17:19:41 -0400

    update file with problem 2

--- higherOrderFunctions.js
+++ higherOrderFunctions.js
@@ -2,3 +2,27 @@ let arrays = [[1, 2, 3], [4, 5], [6]];
 let flattenArray = arrays.reduce((a, b) => a.concat(b) );
 console.log(flattenArray);
 // → [1, 2, 3, 4, 5, 6]
+
+
+
+function loop (value, testFunction, updateFunction, bodyFunction) {
+	let i = 0;
+  	while ( i < value) {
+    	
+    }
+  
+  	testFunction (test, then) {
+    	if(test) then()
+    }
+  
+  	bodyFunction(value) {
+      // takes the value and evaluates the value
+      value
+      
+    }
+  
+  	updateFunction() {
+    	// create new value
+      	// restart from beginning
+    }
+}
